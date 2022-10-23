f3a3a5140f7627df56c234e404f1f46f2f879a10
nissinissinissi <nissisalamat14@icloud.com>
2022-10-23 06:03:55 -0500

    Initial Commit

---
+++ notestest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notestest
+//
+//  Created by User on 28 Tishri 5783.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notestest/notestestApp.swift
@@ -0,0 +1,17 @@
+//
+//  notestestApp.swift
+//  notestest
+//
+//  Created by User on 28 Tishri 5783.
+//
+
+import SwiftUI
+
+@main
+struct notestestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
